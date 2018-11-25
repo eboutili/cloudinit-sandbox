7f675792b4234cccc2593cd48d13f316c660be4a
eboutili <eric.boutilier@puppet.com>
2018-11-25 08:46:09 -0500

    Adding the three files

---
+++ az-vm-instance-create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+az vm create --name UbuntuK8sNode \
+  --resource-group myResourceGroup --size Standard_B2ms --image UbuntuLTS --custom-data ./k8s.txt
+
+az vm create --name CentOSK8sNode \
+  --resource-group myResourceGroup --size Standard_B2ms --image OpenLogic:CentOS:7-CI:latest --custom-data ./k8s.txt
---
+++ do-vm-instance-create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+doctl compute droplet create UbuntuK8sNode \
+  --region sfo2 --size 8gb --image ubuntu-16-04-x64 --user-data-file ./k8s.txt
+
+doctl compute droplet create CentOSK8sNode \
+  --region sfo2 --size 8gb --image centos-7-x64 --user-data-file ./k8s.txt
